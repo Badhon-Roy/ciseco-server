570a4d865e32efd23081ffb343eb485051a9a079
Badhon-Roy <roybadhon286@gmail.com>
2023-12-18 12:27:51 +0600

    show specific categroy products

--- index.js
+++ index.js
@@ -12,49 +12,59 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wj0pjif.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-    const categoryCollection = client.db("cisecoDB").collection("categories");
-
-
-    // category related api
-    app.get('/category',async (req , res)=>{
-        const result = await categoryCollection.find().toArray();
-        res.send(result)
-    })
+        const categoryCollection = client.db("cisecoDB").collection("categories");
+        const productsCollection = client.db("cisecoDB").collection("products");
 
 
+        // category related api
+        app.get('/category', async (req, res) => {
+            const result = await categoryCollection.find().toArray();
+            res.send(result)
+        })
+
+        // products related api
+        app.get('/products', async (req, res) => {
+            let query = {};
+            if (req?.query?.category_name) {
+                query = { category_name: req?.query?.category_name }
+            }
+            const result = await productsCollection.find(query).toArray();
+            res.send(result)
+        })
 
 
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.log);
 
 
 
 app.get('/', (req, res) => {
-  res.send('Ciseco server is running')
+    res.send('Ciseco server is running')
 })
 
 app.listen(port, () => {
-  console.log(`Ciseco server is running on port ${port}`)
+    console.log(`Ciseco server is running on port ${port}`)
 })
