2cb4faef2d051cf0320600728daf01458ba465cd
Badhon-Roy <roybadhon286@gmail.com>
2023-12-21 11:24:35 +0600

    show specific product details

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -45,6 +45,13 @@ async function run() {
             const result = await productsCollection.find(query).toArray();
             res.send(result)
         })
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id : new ObjectId(id)}
+            const result = await productsCollection.findOne(query);
+            res.send(result)
+        })
+
 
         // sort related api
 
